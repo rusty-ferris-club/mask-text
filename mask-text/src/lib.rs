96d997fabf27b9acdc4ce3fb528b2a8a5341e49c
Elad Kaplan <kaplan.elad@gmail.com>
2022-11-01 21:57:41 +0200

    fmt

--- mask-text/src/lib.rs
+++ mask-text/src/lib.rs
@@ -8,13 +8,11 @@
 #![doc = include_str!("../examples/percentage.rs")]
 //! ```
 //! 
-//!
 //! #### Regex:
 //! ```
 #![doc = include_str!("../examples/regex.rs")]
 //! ```
 //! 
-//!
 //! [See more masking option](https://github.com/rusty-ferris-club/mask-text/mask-text/examples/README.md)
 
 pub mod mask;
